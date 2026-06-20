3aa66129669d60e28903696aadcd563688702812
Mirel Volcan <mirelvolcan@MacBook-Pro.local>
2026-06-20 23:31:51 +0200

    Cambios en nombre de variable frontend blueprint

--- app/routes/frontend_routes.py
+++ app/routes/frontend_routes.py
@@ -1,19 +1,19 @@
 from flask import Blueprint, render_template
 
-frontend = Blueprint('frontend', __name__)
+frontend_bp = Blueprint('frontend', __name__)
 
-@frontend.route("/")
+@frontend_bp.route("/")
 def home():
     return render_template("home.html")
 
-@frontend.route("/dashboard")
+@frontend_bp.route("/dashboard")
 def dashboard():
     return render_template("dashboard.html")
 
-@frontend.route("/entity/<name>")
+@frontend_bp.route("/entity/<name>")
 def entity(name):
     return render_template("entity.html", entity=name)
 
-@frontend.route("/login")
+@frontend_bp.route("/login")
 def login():
     return render_template("login.html")
